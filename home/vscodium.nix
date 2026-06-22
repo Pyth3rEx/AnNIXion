876945520cea3355f217b160774167e1ef746fb8
Pyth3rEx <perso@pyth3rex.com>
2026-06-22 09:15:22 +0200

    Minor edit

--- home/vscodium.nix
+++ home/vscodium.nix
@@ -5,47 +5,48 @@
     enable = true;
     package = pkgs.vscodium; # Using VSCodium (open-source) or use pkgs.vscode for proprietary version
 
-    extensions = with pkgs.vscode-extensions; [
-      # Nix Language Support
-      jnoortheen.nix-ide
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        # Nix Language Support
+        jnoortheen.nix-ide
 
-      # Additional useful extensions for Nix development
-      mkhl.direnv
+        # Additional useful extensions for Nix development
+        mkhl.direnv
 
-      # General development tools
-      eamodio.gitlens
-      ms-vscode.makefile-tools
-      tamasfe.even-better-toml
-      redhat.vscode-yaml
-    ];
+        # General development tools
+        eamodio.gitlens
+        ms-vscode.makefile-tools
+        tamasfe.even-better-toml
+        redhat.vscode-yaml
+      ];
+      userSettings = {
+        # Nix IDE Configuration
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+        "nix.linting.enabled" = true;
 
-    userSettings = {
-      # Nix IDE Configuration
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-      "nix.linting.enabled" = true;
+        # Editor settings
+        "[nix]" = {
+          "editor.defaultFormatter" = "jnoortheen.nix-ide";
+          "editor.formatOnSave" = true;
+          "editor.tabSize" = 2;
+          "editor.insertSpaces" = true;
+        };
 
-      # Editor settings
-      "[nix]" = {
-        "editor.defaultFormatter" = "jnoortheen.nix-ide";
-        "editor.formatOnSave" = true;
-        "editor.tabSize" = 2;
-        "editor.insertSpaces" = true;
+        # General VS Code settings
+        "editor.wordWrap" = "on";
+        "editor.formatOnPaste" = true;
+        "files.autoSave" = "afterDelay";
+        "files.autoSaveDelay" = 1000;
+
+        # Git settings
+        "gitlens.hovers.currentLine.enabled" = true;
+        "gitlens.codeLens.enabled" = true;
+
+        # Terminal settings
+        "terminal.integrated.defaultProfile.linux" = "bash";
+        "terminal.integrated.fontFamily" = "monospace";
       };
-
-      # General VS Code settings
-      "editor.wordWrap" = "on";
-      "editor.formatOnPaste" = true;
-      "files.autoSave" = "afterDelay";
-      "files.autoSaveDelay" = 1000;
-
-      # Git settings
-      "gitlens.hovers.currentLine.enabled" = true;
-      "gitlens.codeLens.enabled" = true;
-
-      # Terminal settings
-      "terminal.integrated.defaultProfile.linux" = "bash";
-      "terminal.integrated.fontFamily" = "monospace";
     };
   };
 
