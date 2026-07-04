216526f03ff59f97fda8cb65153059c37390a159
Pyth3rEx <perso@pyth3rex.com>
2026-07-04 20:50:42 +0200

    Config work

--- home/fastfetch.nix
+++ home/fastfetch.nix
@@ -11,34 +11,31 @@
       };
       display = {
         separator = "  ";
+        "constants" = [
+          "──────────────────────────────────"
+          "                                  "
+        ];
         size = {
           binaryPrefix = "si";
         };
         color = {
-          "keys" = "red";
-          "title" = "red";
+          keys = "red";
+          title = "red";
+          custom = "red";
+        };
+        key = {
+          width = 15;
+          type = "both";
         };
       };
       modules = [
-        "title"
-        "separator"
         {
-          "type" = "os";
-          "key" = "OS";
-          "format" = "{name} {version}";
+          "type" = "custom";
+          "format" = "┌─────{$1} GENERAL STATS {$1}────┐";
         }
         {
-          "type" = "kernel";
-          "key" = "Kernel";
-        }
-        {
-          "type" = "memory";
-          "key" = "Memory";
-          "percent" = {
-            "type" = 3;
-            "green" = 30;
-            "yellow" = 70;
-          };
+          "type" = "custom";
+          "format" = "│   {$2}                  {$2}   │";
         }
         {
           type = "datetime";
@@ -50,9 +47,72 @@
           key = "Time";
           format = "{14}:{17}:{20}";
         }
-        "break"
-        "player"
-        "media"
+        {
+          "type" = "custom";
+          "format" = "│   {$2}                  {$2}   │";
+        }
+        {
+          "type" = "custom";
+          "format" = "├{$1} HARDWARE CONFIGURATION {$1}┤";
+        }
+        {
+          "type" = "custom";
+          "format" = "│   {$2}                  {$2}   │";
+        }
+
+        "os"
+        "kernel"
+        "bios"
+        "cpu"
+        "gpu"
+
+        {
+          "type" = "custom";
+          "format" = "│   {$2}                  {$2}   │";
+        }
+        {
+          "type" = "custom";
+          "format" = "├───{$1} USAGE STATISTICS {$1}───┤";
+        }
+        {
+          "type" = "custom";
+          "format" = "│   {$2}                  {$2}   │";
+        }
+        {
+          type = "memory";
+          key = "Memory";
+          percent = {
+            type = 3;
+            green = 30;
+            yellow = 70;
+          };
+        }
+        {
+          type = "disk";
+          key = "Disk";
+          percent = {
+            type = 3;
+            green = 30;
+            yellow = 70;
+          };
+        }
+        {
+          type = "battery";
+          key = "Battery";
+          percent = {
+            type = 3;
+            green = 70;
+            yellow = 30;
+          };
+        }
+        {
+          "type" = "custom";
+          "format" = "│   {$2}                  {$2}   │";
+        }
+        {
+          "type" = "custom";
+          "format" = "└{$1}────────────────────────{$1}┘";
+        }
       ];
     };
   };
