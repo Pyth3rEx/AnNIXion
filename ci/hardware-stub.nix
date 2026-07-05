b9c315a66a23a4f4608b251eefac15dacc7ea06e
Pyth3rEx <perso@pyth3rex.com>
2026-07-05 17:24:42 +0200

    ci: add hardware-stub.nix for local dev and contributor environments

---
+++ ci/hardware-stub.nix
@@ -0,0 +1,29 @@
+{
+  modulesPath,
+  lib,
+  ...
+}:
+{
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  boot.initrd.availableKernelModules = [
+    "ahci"
+    "xhci_pci"
+    "virtio_pci"
+    "sr_mod"
+    "virtio_blk"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+    options = [ "umask=0077" ];
+  };
+  swapDevices = [ ];
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
